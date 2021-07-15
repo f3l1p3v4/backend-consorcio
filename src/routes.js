8ab945b498447f9afb4c407950371ca74dd1353a
Felipe Valdez <47610639+f3l1p3v4@users.noreply.github.com>
2021-07-15 16:21:42 -0400

    update

--- src/routes.js
+++ src/routes.js
@@ -29,16 +29,24 @@ routes.get("/clientes", verifyJWT, (req, res, next) => {
 routes.post("/users/sign_in", (req, res, next) => {
   //esse teste abaixo deve ser feito no seu banco de dados
   console.log(req.body);
-  if (req.body.user === "luiz" && req.body.password === "123") {
+  if (req.body.user === "Felipe" && req.body.password === "123") {
     //auth ok
     const id = 1; //esse id viria do banco de dados
     const token = jwt.sign({ id }, process.env.SECRET, {
       expiresIn: 300 // expires in 5min
     });
-    return res.json({ auth: true, token: token });
-  }
 
-  res.status(500).json({ message: "Login inválido!" + req.body.user });
+    return res.json({ auth: true, token: token, type: "admin" });
+  } else if (req.body.user === "motorista" && req.body.password === "123") {
+    const id = 1; //esse id viria do banco de dados
+    const token = jwt.sign({ id }, process.env.SECRET, {
+      expiresIn: 300 // expires in 5min
+    });
+
+    return res.json({ auth: true, token: token, type: "motorista" });
+  } else {
+    res.status(500).json({ message: "Login inválido!" + req.body.user });
+  }
 });
 
 routes.post("/logout", function (req, res) {
