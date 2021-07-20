d980fe52d110365949bf6438091ad1fffb42b7c5
Felipe Valdez <47610639+f3l1p3v4@users.noreply.github.com>
2021-07-20 09:06:08 -0400

    update

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -6,18 +6,21 @@ module.exports = {
   async authentication(req, res) {
     try {
       const users = await User.find();
-      if (
-        req.body.user === users.name &&
-        req.body.password === users.password
-      ) {
-        //auth ok
-        const id = users._id; //esse id vem do banco de dados
-        const token = jwt.sign({ id }, process.env.SECRET, {
-          expiresIn: 300 // expires in 5min
-        });
-        return res.json({ auth: true, token: token, type: users.type });
-      } else {
-        res.status(500).json({ message: "Login inválido!" });
+
+      for (let i = 0; i < users.length; i++) {
+        if (
+          req.body.user === users[i].name &&
+          req.body.password === users[i].password
+        ) {
+          //auth ok
+          const id = users._id; //esse id vem do banco de dados
+          const token = jwt.sign({ id }, process.env.SECRET, {
+            expiresIn: 300 // expires in 5min
+          });
+          return res.json({ auth: true, token: token, type: users.type });
+        } else {
+          res.status(500).json({ message: "Login inválido!" });
+        }
       }
     } catch (error) {
       res.status(500).send(`Erro ao salvar usuário: ${error}`);
