f5abf022d981eb694a14e6569e1da9bd1a86cce0
Felipe Valdez <47610639+f3l1p3v4@users.noreply.github.com>
2021-08-24 16:53:24 -0400

    update

--- src/controllers/EventController.js
+++ src/controllers/EventController.js
@@ -11,11 +11,10 @@ module.exports = {
 
   async store(req, res) {
     const { filename } = req.file;
-    /*const events = [];
 
     await File.create({
       file: filename
-    });*/
+    });
 
     const workbook = XLSX.readFile(`uploads/${filename}`);
     const sheet_name_list = workbook.SheetNames;
@@ -29,8 +28,43 @@ module.exports = {
 
     const position = xlDataString.length - 15;
 
-    console.log(xlDataString.substr(position));
+    const DateXlsx = xlDataString.substr(position);
 
+    console.log("Data do arquivo xlsx " + xlDataString.substr(position));
+
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    let monthString = JSON.stringify(month);
+
+    let dateCurrent = "";
+
+    if (monthString.length === 1) {
+      dateCurrent = `${day}/0${month}/${year}     `;
+    } else {
+      dateCurrent = `${day}/${month}/${year}     `;
+    }
+
+    if (dateCurrent === DateXlsx) {
+      const events = [];
+
+      for (let i = 0; i < xlData.length; i++) {
+        const event = await Event.create({
+          driver: xlData[i].__EMPTY_1,
+          average: xlData[i].__EMPTY_3
+        });
+
+        events.push(event);
+      }
+      return res.status(200).json(events);
+    } else {
+      res.status(500).json({ message: "Arquivo com data antiga!" });
+    }
+
+    /*
+    --------------------------Trabalhando com datas--------------------------
     var dataAtual = new Date();
     var dia = dataAtual.getDate();
     var mes = dataAtual.getMonth() + 1;
@@ -50,16 +84,6 @@ module.exports = {
         minutos +
         "h."
     );
-
-    /*for (let i = 0; i < xlData.length; i++) {
-      const event = await Event.create({
-        driver: xlData[i].__EMPTY_1,
-        average: xlData[i].__EMPTY_3
-      });
-
-      events.push(event);
-    }
-
-    return res.send(events);*/
+    */
   }
 };
