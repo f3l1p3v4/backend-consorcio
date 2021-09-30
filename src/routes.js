05a8333bd0f4ee69c48cfe04f8252a97e824ab12
Felipe Valdez <47610639+f3l1p3v4@users.noreply.github.com>
2021-09-30 09:26:50 -0400

    added bus

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ const { Router } = require("express");
 const multer = require("multer");
 const uploadConfig = require("./config/upload");
 
+const { verifyJWT } = require("./controllers/AuthController");
 const AuthController = require("./controllers/AuthController");
 const UserController = require("./controllers/UserController");
 const EventController = require("./controllers/EventController");
+const BusController = require("./controllers/BusController");
 
 const routes = Router();
 const upload = multer(uploadConfig);
@@ -22,7 +24,7 @@ routes.post("/logout", function (req, res) {
 });
 
 //Rote Users
-routes.get("/user", UserController.search);
+routes.get("/user", verifyJWT, UserController.search);
 routes.get("/user/driver", UserController.index);
 routes.post("/user", UserController.addUser);
 
@@ -32,5 +34,13 @@ routes.get("/event/driver", EventController.index);
 routes.get("/event/company", EventController.indexCompany);
 routes.get("/event/date", EventController.indexDateCompany);
 routes.post("/event", upload.single("file"), EventController.saveFileXlsx);
+
+//Rote Buses
+routes.get("/all", BusController.search);
+routes.get("/bushes", BusController.searchBushes);
+routes.post("/bushes", BusController.addBushes);
+routes.put("/bushes", BusController.changeStatus);
+routes.put("/bus", BusController.changeBus);
+routes.delete("/bushes/:id", BusController.deleteBus);
 
 module.exports = routes;
